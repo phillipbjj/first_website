61c7dd151d87e69dd2ab976ef1f6ff6dfce362d9
phillipbjj <pjdempsey3@gmail.com>
2023-12-28 10:39:35 -0600

    start of website

---
+++ src/app/bjj/page.js
@@ -0,0 +1,9 @@
+export default function TheGoat() {
+    return (
+        <div>
+        <h1 className="text-xl"> The goats best highlight </h1>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/o1OZHNf8hoo?si=-EWJyLRuclQEZdNn" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullscreen></iframe>
+        </div>
+    )
+}
+
